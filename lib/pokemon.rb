675f4d88ba17d6a47de4fe057dce2964b4fbf357
Chris Junker <elevatemedfit@gmail.com>
2019-08-20 18:07:40 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,6 +10,8 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-      db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", [name, type])
+      db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", [name, type])#Instead, you should use bound parameters for any variable data that you are passing into your SQL statement:
   end
+
+   
 end
