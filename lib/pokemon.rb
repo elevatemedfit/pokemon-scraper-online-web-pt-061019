2ab8146e0a118e18ab707993f62afa8c2efaa1a6
Chris Junker <elevatemedfit@gmail.com>
2019-08-20 18:25:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,13 +13,24 @@ class Pokemon
       db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", [name, type])#Instead, you should use bound parameters for any variable data that you are passing into your SQL statement:
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    type = row[2]
+    self.new(id, name, type)
+  end
+
 def self.find_by_id(id,db)
   sql = <<-SQL
   SELECT * FROM pokemon WHERE id = ?
   LIMIT 1
 
   SQL
-  end
+
+  DB[:conn].execute(sql, id).map do |row|
+    self.new_from_db(row)
+  end.first
+end
 
 end
 
